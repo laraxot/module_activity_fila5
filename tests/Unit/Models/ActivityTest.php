2dce62d264a8f92a1b2166e5fe83a36c8c9059b3
marco76tv <marco.sottana@gmail.com>
2026-03-02 21:39:56 +0100

    .

--- tests/Unit/Models/ActivityTest.php
+++ tests/Unit/Models/ActivityTest.php
@@ -13,6 +13,7 @@ test('activity model can be created', function () {
 });
 
 test('activity model can be saved and retrieved', function () {
+    dd(DB::connection('activity')->getDatabaseName());
     $activity = Activity::factory()->create([
         'description' => 'Test action',
         'event' => 'test_event',
