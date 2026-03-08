1c2d33b7510553de95fd18a432267c6cd58e13d0
marco76tv <marco.sottana@gmail.com>
2026-03-08 11:12:36 +0100

    .

--- tests/Feature/FilamentTest.php
+++ tests/Feature/FilamentTest.php
@@ -62,7 +62,7 @@ describe('ListLogActivitiesAction', function (): void {
             protected function setUp(): void
             {
                 parent::setUp();
-                // @var mixed iconButton(;
+                $this->iconButton();
             }
         };
         
