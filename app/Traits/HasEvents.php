d1e6019d535be2c3b2e036b1075ac3ab248d6ebc
marco76tv <marco.sottana@gmail.com>
2026-03-09 08:49:30 +0100

    .

--- app/Traits/HasEvents.php
+++ app/Traits/HasEvents.php
@@ -15,7 +15,7 @@ trait HasEvents
      */
     public function storedEvents(): MorphMany
     {
-        return // @var mixed morphMany(StoredEvent::class, 'aggregate';
+        return $this->morphMany(StoredEvent::class, 'aggregate');
     }
 
     /**
@@ -23,6 +23,6 @@ trait HasEvents
      */
     public function snapshots(): MorphMany
     {
-        return // @var mixed morphMany(Snapshot::class, 'aggregate';
+        return $this->morphMany(Snapshot::class, 'aggregate');
     }
 }
