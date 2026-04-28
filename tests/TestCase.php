9a5a9ac33ad1177b00bf782ef5800f295c06e7ff
marco76tv <marco.sottana@gmail.com>
2026-04-28 16:54:14 +0200

    .

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Modules\Activity\Tests;
 
+<<<<<<< HEAD
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+=======
+>>>>>>> a21dc33d (.)
 use Modules\Xot\Tests\XotBaseTestCase;
 
 /**
@@ -12,6 +15,7 @@ use Modules\Xot\Tests\XotBaseTestCase;
  *
  * Uses MySQL from .env.testing (carbon copy of .env with _test DB names).
  * All module connections are mapped dynamically by TenantServiceProvider.
+<<<<<<< HEAD
  * Migrations must be run ONCE externally: php artisan migrate --env=testing
  * DB lifecycle is managed by dedicated integration tests/configuration.
  */
@@ -26,6 +30,12 @@ abstract class TestCase extends XotBaseTestCase
      */
     protected array $connectionsToTransact = ['mysql', 'activity', 'user'];
 
+=======
+ * Migrations are run ONCE automatically via XotBaseTestCase.
+ */
+abstract class TestCase extends XotBaseTestCase
+{
+>>>>>>> a21dc33d (.)
     /**
      * @return array<int, class-string>
      */
@@ -43,7 +53,10 @@ abstract class TestCase extends XotBaseTestCase
      */
     protected function defineEnvironment($app): void
     {
+<<<<<<< HEAD
         // Use mysql for Activity to ensure test DB has the table
+=======
+>>>>>>> a21dc33d (.)
         $app['config']->set('activitylog.database_connection', 'mysql');
     }
 
@@ -54,8 +67,11 @@ abstract class TestCase extends XotBaseTestCase
     {
         parent::setUp();
 
+<<<<<<< HEAD
         // Reset the model to ensure it uses the correct connection
         // This is needed because Activity has $connection = 'activity' by default
+=======
+>>>>>>> a21dc33d (.)
         $this->app->forgetInstance(\Modules\Activity\Models\Activity::class);
     }
 }
