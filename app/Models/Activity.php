c2ea7264bfee9af88fef7f4c169e9a4d11160f43
marco76tv <marco.sottana@gmail.com>
2026-04-28 16:43:37 +0200

    .

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -21,7 +21,7 @@ use Spatie\Activitylog\Models\Activity as SpatieActivity;
  * @property string|null $log_name
  * @property string $description
  * @property string|null $subject_type
- * @property int|null $subject_id
+ * @property string|null $subject_id
  * @property string|null $causer_type
  * @property string|null $causer_id
  * @property array<string, mixed>|Collection<array-key, mixed>|null $properties
@@ -106,6 +106,8 @@ class Activity extends SpatieActivity
     use HasXotFactory;
 
     protected $connection = 'activity';
+
+    
 
     /** @var list<string> */
     protected $fillable = [
