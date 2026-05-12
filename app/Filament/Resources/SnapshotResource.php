348c7a9168d5fbdacdc863033f523280323a5867
marco76tv <marco.sottana@gmail.com>
2026-05-12 08:09:46 +0200

    .

--- app/Filament/Resources/SnapshotResource.php
+++ app/Filament/Resources/SnapshotResource.php
@@ -4,10 +4,19 @@ declare(strict_types=1);
 
 namespace Modules\Activity\Filament\Resources;
 
+use Modules\Activity\Filament\Resources\SnapshotResource\Schemas\SnapshotForm;
 use Modules\Activity\Models\Snapshot;
 use Modules\Xot\Filament\Resources\XotBaseResource;
 
 class SnapshotResource extends XotBaseResource
 {
     protected static ?string $model = Snapshot::class;
+
+    /**
+     * @return array<int|string, \Filament\Forms\Components\Component>
+     */
+    public static function getFormSchema(): array
+    {
+        return SnapshotForm::getFormSchema();
+    }
 }
--- app/Filament/Resources/StoredEventResource.php
+++ app/Filament/Resources/StoredEventResource.php
@@ -4,10 +4,19 @@ declare(strict_types=1);
 
 namespace Modules\Activity\Filament\Resources;
 
+use Modules\Activity\Filament\Resources\StoredEventResource\Schemas\StoredEventForm;
 use Modules\Activity\Models\StoredEvent;
 use Modules\Xot\Filament\Resources\XotBaseResource;
 
 class StoredEventResource extends XotBaseResource
 {
     protected static ?string $model = StoredEvent::class;
+
+    /**
+     * @return array<int|string, \Filament\Forms\Components\Component>
+     */
+    public static function getFormSchema(): array
+    {
+        return StoredEventForm::getFormSchema();
+    }
 }
