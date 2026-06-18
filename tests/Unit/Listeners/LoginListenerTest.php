facf70df40bfde517250b386838f62d17315b033
Malebestia <malebestia@yahoo.it>
2026-06-18 18:23:40 +0200

    .

--- tests/Unit/Listeners/LoginListenerTest.php
+++ tests/Unit/Listeners/LoginListenerTest.php
@@ -2,8 +2,6 @@
 
 declare(strict_types=1);
 
-namespace Modules\Activity\Tests\Unit\Listeners;
-
 use Illuminate\Auth\Events\Login;
 use Modules\Activity\Providers\EventServiceProvider;
 use Modules\Activity\Listeners\LoginListener;
