cfb6d83f2b9003eff4af295ae2b0a0a28142f546
Malebestia <malebestia@yahoo.it>
2026-06-18 17:52:57 +0200

    .

--- workbench/routes/console.php
+++ workbench/routes/console.php
@@ -14,5 +14,5 @@ declare(strict_types=1);
 */
 
 // Artisan::command('inspire', function () {
-//     $this->comment(Inspiring::quote();
+//     $this->comment(Inspiring::quote());
 // })->purpose('Display an inspiring quote');
