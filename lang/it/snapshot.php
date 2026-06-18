6bc2b4d13e3766f79c95a2686b56356d9fc98f5c
Malebestia <malebestia@yahoo.it>
2026-06-18 18:18:27 +0200

    .

--- lang/it/snapshot.php
+++ lang/it/snapshot.php
@@ -41,24 +41,6 @@ return [
         'delete' => [
             'label' => 'Elimina Snapshot',
         ],
-        'id' => [
-            'label' => 'id',
-        ],
-        'aggregate_uuid' => [
-            'label' => 'aggregate_uuid',
-        ],
-        'aggregate_version' => [
-            'label' => 'aggregate_version',
-        ],
-        'created_at' => [
-            'label' => 'created_at',
-        ],
-        'updated_at' => [
-            'label' => 'updated_at',
-        ],
-        'aggregate_type' => [
-            'label' => 'aggregate_type',
-        ],
     ],
     'label' => 'Snapshot',
     'plural_label' => 'Snapshot (Plurale)',
@@ -68,18 +50,9 @@ return [
         ],
         'edit' => [
             'label' => 'Modifica Snapshot',
-            'icon' => 'edit',
-            'tooltip' => 'edit',
         ],
         'delete' => [
             'label' => 'Elimina Snapshot',
-            'icon' => 'delete',
-            'tooltip' => 'delete',
-        ],
-        'view' => [
-            'label' => 'view',
-            'icon' => 'view',
-            'tooltip' => 'view',
         ],
     ],
     'label' => 'Snapshot',
