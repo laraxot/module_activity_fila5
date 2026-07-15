51856c66985e50e20e703354aaa72ea1a4549015
marco76tv <marco.sottana@gmail.com>
2026-07-15 14:35:24 +0200

    .

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -39,7 +39,6 @@ use Spatie\SchemalessAttributes\Casts\SchemalessAttributes;
  * @property-read Model|null $causer
  * @property-read Collection<int, mixed> $changes
  * @property-read Model|null $subject
- *
  * @method static ActivityFactory factory($count = null, $state = [])
  * @method static Builder<static>|Activity forBatch(string $batchUuid)
  * @method static Builder<static>|Activity forEvent(string $event)
@@ -103,7 +102,6 @@ use Spatie\SchemalessAttributes\Casts\SchemalessAttributes;
  * @method static Builder<static>|Activity rightJoin(string $table, string $first, string $operator = null, string $second = null)
  * @method static Builder<static>|Activity crossJoin(string $table)
  * @method static Builder<static>|Activity causedBy(Model $causer)
- *
  * @mixin \Eloquent
  */
 class Activity extends SpatieActivity
