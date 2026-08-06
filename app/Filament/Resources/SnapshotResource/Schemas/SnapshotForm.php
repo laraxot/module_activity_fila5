c4bf9031e98fa15c47c452bc4c35ca06cfb3e9be
Marco Xot <marco76tv@hotmail.com>
2026-08-06 19:44:18 +0200

    .

--- app/Filament/Resources/SnapshotResource/Schemas/SnapshotForm.php
+++ app/Filament/Resources/SnapshotResource/Schemas/SnapshotForm.php
@@ -14,7 +14,7 @@ class SnapshotForm extends XotBaseResourceForm
     /**
      * @return array<string, Component>
      */
-    public static function getFormSchema(): array
+    public static function getFormSchemaOld(): array
     {
         return [
             'aggregate_uuid' => TextInput::make('aggregate_uuid')
