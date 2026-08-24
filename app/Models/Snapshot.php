4b77f576ada37d4877b8c61ded82f1f8dee71106
Marco Xot <marco76tv@hotmail.com>
2026-08-24 16:57:35 +0200

    .

--- app/Models/Snapshot.php
+++ app/Models/Snapshot.php
@@ -15,30 +15,30 @@ use Spatie\EventSourcing\Snapshots\EloquentSnapshot as SpatieSnapshot;
  * @property string $aggregate_uuid
  * @property int $aggregate_version
  * @property array<array-key, mixed> $state
- * @property Carbon|null $created_at
- * @property Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
  * @property string|null $updated_by
  * @property string|null $created_by
  *
- * @method static Builder<static>|Snapshot newModelQuery()
- * @method static Builder<static>|Snapshot newQuery()
- * @method static Builder<static>|Snapshot query()
- * @method static Builder<static>|Snapshot uuid(string $uuid)
- * @method static Builder<static>|Snapshot whereAggregateUuid($value)
- * @method static Builder<static>|Snapshot whereAggregateVersion($value)
- * @method static Builder<static>|Snapshot whereCreatedAt($value)
- * @method static Builder<static>|Snapshot whereCreatedBy($value)
- * @method static Builder<static>|Snapshot whereId($value)
- * @method static Builder<static>|Snapshot whereState($value)
- * @method static Builder<static>|Snapshot whereUpdatedAt($value)
- * @method static Builder<static>|Snapshot whereUpdatedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Snapshot newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Snapshot newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Snapshot query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Snapshot uuid(string $uuid)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Snapshot whereAggregateUuid($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Snapshot whereAggregateVersion($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Snapshot whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Snapshot whereCreatedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Snapshot whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Snapshot whereState($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Snapshot whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Snapshot whereUpdatedBy($value)
  * @method static SnapshotFactory factory($count = null, $state = [])
  *
  * @mixin \Eloquent
  */
 class Snapshot extends SpatieSnapshot
 {/**
- * @phpstan-use HasXotFactory<\Modules\Activity\Database\Factories\SnapshotFactory, self>
+ * @phpstan-use HasXotFactory<\Modules\Activity\Database\Factories\SnapshotFactory>
  */
 use HasXotFactory;
 
