d8a1642d06cb96e9fbb045c02f054151376ba75d
Marco Xot <marco76tv@hotmail.com>
2026-09-11 16:02:34 +0200

    .

--- app/Filament/Resources/SnapshotResource/Tables/SnapshotsTable.php
+++ app/Filament/Resources/SnapshotResource/Tables/SnapshotsTable.php
@@ -6,22 +6,29 @@ namespace Modules\Activity\Filament\Resources\SnapshotResource\Tables;
 
 use Filament\Tables\Columns\Column;
 use Filament\Tables\Columns\TextColumn;
+use Modules\Activity\Models\Snapshot;
 use Modules\Xot\Filament\Resources\Tables\XotBaseResourceTable;
+
+use function Safe\json_encode;
 
 class SnapshotsTable extends XotBaseResourceTable
 {
     /**
-     * @return array<int|string, Column>
+     * @return array<string, Column>
      */
     public function getTableColumns(): array
     {
         return [
-            'id' => TextColumn::make('id')->searchable()->sortable(),
-            'aggregate_uuid' => TextColumn::make('aggregate_uuid')->searchable()->limit(30),
-            'aggregate_version' => TextColumn::make('aggregate_version')->searchable()->sortable(),
-            'state' => TextColumn::make('state')->limit(50),
-            'created_at' => TextColumn::make('created_at')->dateTime(),
-            'updated_at' => TextColumn::make('updated_at')->dateTime(),
+            'aggregate_uuid' => TextColumn::make('aggregate_uuid')->searchable()->sortable()->copyable(),
+            'aggregate_version' => TextColumn::make('aggregate_version')->numeric()->sortable(),
+            'state' => TextColumn::make('state')
+                ->state(static fn (Snapshot $record): string => json_encode($record->state, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES))
+                ->limit(100)
+                ->wrap()
+                ->toggleable(isToggledHiddenByDefault: true),
+            'created_at' => TextColumn::make('created_at')->dateTime()->sortable(),
+            'updated_at' => TextColumn::make('updated_at')->dateTime()->sortable()->toggleable(isToggledHiddenByDefault: true),
+            'id' => TextColumn::make('id')->sortable()->toggleable(isToggledHiddenByDefault: true),
         ];
     }
 }
--- app/Filament/Resources/StoredEventResource/Tables/StoredEventsTable.php
+++ app/Filament/Resources/StoredEventResource/Tables/StoredEventsTable.php
@@ -6,21 +6,30 @@ namespace Modules\Activity\Filament\Resources\StoredEventResource\Tables;
 
 use Filament\Tables\Columns\Column;
 use Filament\Tables\Columns\TextColumn;
+use Modules\Activity\Models\StoredEvent;
 use Modules\Xot\Filament\Resources\Tables\XotBaseResourceTable;
+
+use function Safe\json_encode;
 
 class StoredEventsTable extends XotBaseResourceTable
 {
     /**
-     * @return array<int|string, Column>
+     * @return array<string, Column>
      */
     public function getTableColumns(): array
     {
         return [
-            'id' => TextColumn::make('id')->searchable()->sortable(),
-            'event_class' => TextColumn::make('event_class')->searchable(),
-            'properties' => TextColumn::make('properties')->limit(50),
-            'created_at' => TextColumn::make('created_at')->dateTime(),
-            'updated_at' => TextColumn::make('updated_at')->dateTime(),
+            'event_class' => TextColumn::make('event_class')->searchable()->sortable()->wrap(),
+            'aggregate_uuid' => TextColumn::make('aggregate_uuid')->searchable()->sortable()->copyable(),
+            'aggregate_version' => TextColumn::make('aggregate_version')->numeric()->sortable(),
+            'event_version' => TextColumn::make('event_version')->numeric()->sortable(),
+            'event_properties' => TextColumn::make('event_properties')
+                ->state(static fn (StoredEvent $record): string => json_encode($record->event_properties, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES))
+                ->limit(100)
+                ->wrap()
+                ->toggleable(isToggledHiddenByDefault: true),
+            'created_at' => TextColumn::make('created_at')->dateTime()->sortable(),
+            'id' => TextColumn::make('id')->sortable()->toggleable(isToggledHiddenByDefault: true),
         ];
     }
 }
