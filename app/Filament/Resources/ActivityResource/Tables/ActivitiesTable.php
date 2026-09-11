45201b8e1e1cb3b95984bda37d1d37eed185ed49
Marco Xot <marco76tv@hotmail.com>
2026-09-11 16:02:34 +0200

    .

--- app/Filament/Resources/ActivityResource/Tables/ActivitiesTable.php
+++ app/Filament/Resources/ActivityResource/Tables/ActivitiesTable.php
@@ -6,27 +6,34 @@ namespace Modules\Activity\Filament\Resources\ActivityResource\Tables;
 
 use Filament\Tables\Columns\Column;
 use Filament\Tables\Columns\TextColumn;
+use Modules\Activity\Models\Activity;
 use Modules\Xot\Filament\Resources\Tables\XotBaseResourceTable;
+
+use function Safe\json_encode;
 
 class ActivitiesTable extends XotBaseResourceTable
 {
     /**
-     * @return array<int|string, Column>
+     * @return array<string, Column>
      */
     public function getTableColumns(): array
     {
         return [
             'id' => TextColumn::make('id')->searchable()->sortable(),
             'log_name' => TextColumn::make('log_name')->searchable(),
-            'description' => TextColumn::make('description')->searchable(),
+            'description' => TextColumn::make('description')->searchable()->wrap()->limit(100),
             'event' => TextColumn::make('event')->searchable(),
-            'subject_type' => TextColumn::make('subject_type')->searchable(),
-            'subject_id' => TextColumn::make('subject_id')->searchable(),
-            'causer_type' => TextColumn::make('causer_type')->searchable(),
-            'causer_id' => TextColumn::make('causer_id')->searchable(),
-            'properties' => TextColumn::make('properties')->limit(50),
-            'batch_uuid' => TextColumn::make('batch_uuid')->limit(30),
-            'created_at' => TextColumn::make('created_at')->dateTime(),
+            'subject_type' => TextColumn::make('subject_type')->searchable()->toggleable(isToggledHiddenByDefault: true),
+            'subject_id' => TextColumn::make('subject_id')->searchable()->toggleable(isToggledHiddenByDefault: true),
+            'causer_type' => TextColumn::make('causer_type')->searchable()->toggleable(isToggledHiddenByDefault: true),
+            'causer_id' => TextColumn::make('causer_id')->searchable()->toggleable(isToggledHiddenByDefault: true),
+            'batch_uuid' => TextColumn::make('batch_uuid')->limit(30)->toggleable(isToggledHiddenByDefault: true),
+            'properties' => TextColumn::make('properties')
+                ->state(static fn (Activity $record): string => json_encode($record->properties, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES))
+                ->limit(100)
+                ->wrap()
+                ->toggleable(isToggledHiddenByDefault: true),
+            'created_at' => TextColumn::make('created_at')->dateTime()->sortable(),
             'updated_at' => TextColumn::make('updated_at')->dateTime(),
         ];
     }
--- app/Filament/Resources/ActivityResource/Tables/ActivitysTable.php
+++ app/Filament/Resources/ActivityResource/Tables/ActivitysTable.php
@@ -4,21 +4,22 @@ declare(strict_types=1);
 
 namespace Modules\Activity\Filament\Resources\ActivityResource\Tables;
 
+use Filament\Tables\Columns\Column;
 use Filament\Tables\Columns\TextColumn;
 use Modules\Xot\Filament\Resources\Tables\XotBaseResourceTable;
 
 class ActivitysTable extends XotBaseResourceTable
 {
     /**
-     * @return array<string, TextColumn>
+     * @return array<string, Column>
      */
     public function getTableColumns(): array
     {
         return [
             'id' => TextColumn::make('id')->searchable()->sortable(),
             'log_name' => TextColumn::make('log_name')->searchable(),
-            'description' => TextColumn::make('description')->searchable(),
-            'created_at' => TextColumn::make('created_at')->dateTime(),
+            'description' => TextColumn::make('description')->searchable()->wrap()->limit(100),
+            'created_at' => TextColumn::make('created_at')->dateTime()->sortable(),
         ];
     }
 }
